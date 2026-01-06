a2d25215ce30d33e5249ce5154f1ec9d707098e2
THARINDUnirmal <tharinduna0@gmail.com>
2026-01-06 10:15:27 +0530

    create main components

---
+++ src/app/pages/event-listing/event-listing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-listing',
+  imports: [],
+  templateUrl: './event-listing.html',
+  styleUrl: './event-listing.css',
+})
+export class EventListing {
+
+}
