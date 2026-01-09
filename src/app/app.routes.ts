0e8f1dd44a6274f0efa5a7d98c0a61f7eeb4184a
THARINDUnirmal <tharinduna0@gmail.com>
2026-01-09 11:10:14 +0530

    bug fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { Home } from './pages/home/home';
 import { EventListing } from './pages/event-listing/event-listing';
 import { About } from './pages/about/about';
 import { ContactUs } from './pages/contact-us/contact-us';
+import { RegistrationComponent } from './pages/registration/registration';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'events', component: EventListing },
     { path: 'about', component: About },
     { path: 'contact', component: ContactUs },
-    // { path: 'events', component: EventListing },
+    { path: 'Login', component: RegistrationComponent },
     //{ path: 'events/:id', component: EventDetails },
     // { path: 'create-event', component:  },
     // { path: 'login', component: LoginComponent },
