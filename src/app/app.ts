c7846ad4f64f0abcb5f5e283ff8b664e4a64fcc2
Tharindu Nirmal <tharinduna0@gmail.com>
2026-01-09 10:17:37 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RegistrationComponent } from './pages/registration/registration';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RegistrationComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
