291971a12cda8ab82de4d62a68780a54ddf5fbd4
THARINDUnirmal <tharinduna0@gmail.com>
2026-01-22 10:39:10 +0530

    all update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { EventListing } from './pages/event-listing/event-listing';
 import { About } from './pages/about/about';
 import { ContactUs } from './pages/contact-us/contact-us';
 import { RegistrationComponent } from './pages/registration/registration';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'events', component: EventListing },
     { path: 'about', component: About },
     { path: 'contact', component: ContactUs },
-    { path: 'Login', component: RegistrationComponent },
+    //{ path: 'Login', component: RegistrationComponent },
+    { path: 'Login', component: AdminDashboard },
     //{ path: 'events/:id', component: EventDetails },
     // { path: 'create-event', component:  },
     // { path: 'login', component: LoginComponent },
