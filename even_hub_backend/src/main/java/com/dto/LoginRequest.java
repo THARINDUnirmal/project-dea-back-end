6f494067cb4d56c643ca1f73b5792947bf5a04c5
kusalya2003 <kusalyadissanayake@gmail.com>
2026-02-13 15:26:16 +0530

    login dto implement - ODKT Dissanayake (36781)

---
+++ even_hub_backend/src/main/java/com/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.example.even_hub_backend.dto;
+
+public class LoginRequest {
+    private String userName;
+    private String password;
+
+    // Getters and Setters
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
