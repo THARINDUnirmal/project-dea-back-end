faf87d503901bd124af2a35a79d186825d1be197
kusalya2003 <kusalyadissanayake@gmail.com>
2026-02-13 15:56:36 +0530

    jwutils -ODKT Dissanayake (36781)

---
+++ even_hub_backend/src/main/java/com/example/even_hub_backend/security/JwtUtils.java
@@ -0,0 +1,42 @@
+package com.example.even_hub_backend.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    
+    private final String jwtSecret = "eventHubSecretKey012345678901234567890123456789";
+    private final int jwtExpirationMs = 86400000;
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateTokenFromUsername(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
