f644060e24233b7413e76de82870276905afbd6a
nayanajith2002 <purnanayanajith9@gmail.com>
2026-02-13 14:20:07 +0530

    user repository push-poorna nayanajith (37228)

---
+++ even_hub_backend/src/main/java/com/example/even_hub_backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.even_hub_backend.repository;
+
+import com.example.even_hub_backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+
+    Optional<User> findByUserEmail(String userEmail);
+}
