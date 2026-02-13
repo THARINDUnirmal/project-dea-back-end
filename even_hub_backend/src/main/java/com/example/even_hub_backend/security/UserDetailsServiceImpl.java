15f4341f20f7ed59950174b80ef52cd27e892e35
THARINDUnirmal <tharinduna0@gmail.com>
2026-02-13 15:48:33 +0530

    user details servicelmpl implement - K T N Ranshika (36874)

---
+++ even_hub_backend/src/main/java/com/example/even_hub_backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.even_hub_backend.security;
+
+import com.example.even_hub_backend.models.User;
+import com.example.even_hub_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().toUpperCase())
+        );
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                authorities
+        );
+    }
+}
