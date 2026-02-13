397ee50f5d74d56d2c8bf80d37d958c069a533d6
THARINDUnirmal <tharinduna0@gmail.com>
2026-02-13 13:01:45 +0530

    project files push - K T N Ranshika (36874)

---
+++ even_hub_backend/src/main/java/com/example/even_hub_backend/EvenHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.even_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenHubBackendApplication.class, args);
+	}
+
+}
---
+++ even_hub_backend/src/test/java/com/example/even_hub_backend/EvenHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.even_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
