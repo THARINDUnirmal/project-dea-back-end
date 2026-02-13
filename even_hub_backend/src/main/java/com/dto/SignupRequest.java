35544513bd4ca99dbed4faa32dcf31da345304e2
ridmiimesha96-hue <ridmiimesha96@gmail.com>
2026-02-13 15:23:35 +0530

    SignupRequest push jmriwijesiri(37008)

---
+++ even_hub_backend/src/main/java/com/dto/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.example.even_hub_backend.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    private String userName;
+    private String userEmail;
+    private String password;
+    private String role; // ex: admin, user
+}
